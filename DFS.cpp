48d0abbc56936a4b81ee7412963115d2d7b20c3a
PilferAhn <49383308+PilferAhn@users.noreply.github.com>
2019-08-19 22:30:28 +0900

    Create DFS.cpp

---
+++ DFS.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 9 12
+// 1 2
+// 1 3 
+// 2 3 
+// 2 4 
+// 2 6 
+// 3 7 
+// 4 5
+// 4 7
+// 4 8
+// 5 6 
+// 7 8 
+// 8 9
+
+const int MAX = 100;
+int n,m;
+vector <int> v[MAX];
+bool visited[MAX];
+
+void DFS(int x){
+  
+  // x부터 시작하여 DFS하여 그 순서를 출력하는 상수.
+  // visited 에 방문 기록이 있음. 
+  
+  visited[x] = true;
+  cout << x << " ";
+  
+  for(int i=0;i<v[x].size();i++){
+    int y = v[x][i];
+    // x와 y가 연결되어있다.
+    
+    if(visited[y] == false){
+      DFS(y);
+    }
+    
+  }
+}
+
+
+int main(){
+  
+  cin >> n >> m;
+  
+  for(int i=0;i<m;i++){
+    
+    int a,b;
+    cin >> a >> b;
+    
+    v[a].push_back(b);
+    v[b].push_back(a);
+    
+  }
+  
+  DFS(1);
+  
+  return 0;
+}
