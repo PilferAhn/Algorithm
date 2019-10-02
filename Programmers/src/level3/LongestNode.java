cb7f498091c65e39b23a90f5811da11d4b27700a
PilferAhn <49383308+PilferAhn@users.noreply.github.com>
2019-10-02 09:37:06 +0900

    No progresss shit..
    maybe 2 class was added however, failed to find the soltuion.

--- Programmers/src/level3/LongestNode.java
+++ Programmers/src/level3/LongestNode.java
@@ -1,18 +1,41 @@
 package level3;
-import java.util.*;
+//import java.util.*;
 public class LongestNode {
-	static Queue<Integer> q = new LinkedList<Integer>();
-	static int n = 7;
+	static boolean[] check = new boolean[100];
+//	static Queue<Integer> q = new LinkedList<Integer>();
 	static int answer = 0;
-	static boolean[] check= new boolean[20000];
-	
+	public static void dfs(int[][] tree, int node, int depth) {
+		
+		answer = Math.max(depth, answer);
+		System.out.print(depth + " ");
+		for(int i = 1 ; i < 7; i++) {
+			if(check[i] == false) {
+				check[i] = true;
+				
+				if(tree[node][i] == 1 && check[node] == true) {
+					
+					dfs(tree, i, depth + 1);
+					depth--;
+					check[i] = false;
+				}
+				
+				
+				
+				
+			}
+		}
+		
+		
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
+		 
 		int[][] tree = new int[20000][20000];
 		int[][] node = {{3,6}, {4,3}, {3,2}, {1,3}, {1,2}, {2,4}, {5,2}};
-		
+
 		for(int i = 0; i < 7; i++) {
 			int x = node[i][1];
 			int y = node[i][0];
@@ -21,31 +44,14 @@ public class LongestNode {
 //			System.out.println(tree[x][y]);
 		}
 		
+	
+		check[1] = true;
+		
+		dfs(tree, 1, 0);
 		
 		
+		System.out.print(answer);
 		
-//		q.add(1);
-//		check[1] = true;
-//		
-//		while(!q.isEmpty()) {
-//			
-//			int temp = q.poll();
-//			answer++;
-//			for(int i = 1; i <= 6; i++) {
-//				if(check[i] == false && tree[temp][i] == 1) {
-//					check[i] = true;
-//					System.out.print(i + " ");
-//					q.add(i);
-//					
-//				}
-//				
-//			}
-//			
-//		}
-		
-//		check[1] = true;
-		
-		System.out.print("Answer is " + answer);
 		
 	}
 
