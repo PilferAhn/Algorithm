5dd4c491e143d349fbc206579af054e6ffddc251
Jimin Ahn <30714171+rmit-s3560873-jimin-ahn@users.noreply.github.com>
2019-10-11 02:02:00 +0900

    gg fuck up

--- Programmers/src/level3/LongestNode.java
+++ Programmers/src/level3/LongestNode.java
@@ -1,31 +1,33 @@
 package level3;
-//import java.util.*;
+import java.util.*;
 public class LongestNode {
-	static boolean[] check = new boolean[100];
-//	static Queue<Integer> q = new LinkedList<Integer>();
+	
+	static boolean[] check;
+	static int[] parent;
 	static int answer = 0;
-	public static void dfs(int[][] tree, int node, int depth) {
+	static int[] tempNode;
+	public static void dfs(int[][] tree, int node, int len, int previous, int depth) {
 		
-		answer = Math.max(depth, answer);
-		System.out.print(depth + " ");
-		for(int i = 1 ; i < 7; i++) {
+		answer = Math.max(answer, depth);
+		
+		for(int i = 1; i < len; i++) {
+			
 			if(check[i] == false) {
 				check[i] = true;
-				
-				if(tree[node][i] == 1 && check[node] == true) {
+				if(tree[node][i] == 1 && parent[previous] != 0) {
 					
-					dfs(tree, i, depth + 1);
+					parent[node] = 1;
+					tempNode[node] = depth;
+					dfs(tree, i, len, node, depth+1);
 					depth--;
+					parent[node] = 0;
 					check[i] = false;
+					
+					
 				}
-				
-				
-				
-				
 			}
+			
 		}
-		
-		
 		
 	}
 
@@ -33,24 +35,29 @@ public class LongestNode {
 		// TODO Auto-generated method stub
 		//[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
 		 
+		int[][] edges = {{3,6}, {4,3}, {3,2}, {1,3}, {1,2}, {2,4}, {5,2}};
+		
+		int n = 6;
+		
+		check = new boolean[n+1];
+		parent = new int[n+1];
+		tempNode = new int[n+1];
 		int[][] tree = new int[20000][20000];
-		int[][] node = {{3,6}, {4,3}, {3,2}, {1,3}, {1,2}, {2,4}, {5,2}};
-
-		for(int i = 0; i < 7; i++) {
-			int x = node[i][1];
-			int y = node[i][0];
+		for(int i = 0; i < edges.length; i++) {
 			
-			tree[x][y] = tree[y][x] = 1;
-//			System.out.println(tree[x][y]);
+			int x = edges[i][0];
+			int y = edges[i][1];
+			
+			tree[i][0] = tree[0][i] = 1;
+			tree[i][1] = tree[1][i] = 1;
 		}
 		
-	
-		check[1] = true;
+		check[1] = true; 
+//		System.out.print(parent[1]);
 		
-		dfs(tree, 1, 0);
-		
-		
-		System.out.print(answer);
+		for(int deep : tempNode) {
+			System.out.print(deep + " ");
+		}
 		
 		
 	}
