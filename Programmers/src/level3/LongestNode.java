cdd1b9b8a331227d7cc2e8c48b0edc6206632ec1
Jimin Ahn <30714171+rmit-s3560873-jimin-ahn@users.noreply.github.com>
2019-10-12 11:44:45 +0900

    bit progress

--- Programmers/src/level3/LongestNode.java
+++ Programmers/src/level3/LongestNode.java
@@ -2,64 +2,68 @@ package level3;
 import java.util.*;
 public class LongestNode {
 	
-	static boolean[] check;
-	static int[] parent;
-	static int answer = 0;
-	static int[] tempNode;
-	public static void dfs(int[][] tree, int node, int len, int previous, int depth) {
-		
-		answer = Math.max(answer, depth);
-		
-		for(int i = 1; i < len; i++) {
-			
-			if(check[i] == false) {
-				check[i] = true;
-				if(tree[node][i] == 1 && parent[previous] != 0) {
-					
-					parent[node] = 1;
-					tempNode[node] = depth;
-					dfs(tree, i, len, node, depth+1);
-					depth--;
-					parent[node] = 0;
-					check[i] = false;
-					
-					
-				}
-			}
-			
-		}
-		
-	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
-		 
+		int answer = 0;
 		int[][] edges = {{3,6}, {4,3}, {3,2}, {1,3}, {1,2}, {2,4}, {5,2}};
-		
 		int n = 6;
 		
-		check = new boolean[n+1];
-		parent = new int[n+1];
-		tempNode = new int[n+1];
-		int[][] tree = new int[20000][20000];
+		ArrayList<Integer>[] tree = (ArrayList<Integer>[]) new ArrayList[n+1];
+		int[] distance = new int[n+1];
+		
+		int max = 0;
+		Queue <Integer> q = new LinkedList<Integer>();
+		boolean check[] = new boolean[n+1];
+		
+		for(int i = 1; i <= n; i++) {
+			tree[i] = new ArrayList<>();
+		}
+		
 		for(int i = 0; i < edges.length; i++) {
-			
-			int x = edges[i][0];
-			int y = edges[i][1];
-			
-			tree[i][0] = tree[0][i] = 1;
-			tree[i][1] = tree[1][i] = 1;
+			tree[edges[i][0]].add(edges[i][1]);
+			tree[edges[i][1]].add(edges[i][0]);
 		}
 		
+//		for(int temp : tree[2]) System.out.print(temp + " ");
+//		System.out.println();
+//		
+//		
+//		for(int temp : tree[3]) System.out.print(temp + " ");
+//		System.out.println();
+		
+		q.add(1); 
 		check[1] = true; 
-//		System.out.print(parent[1]);
+		distance[1] = 0;
 		
-		for(int deep : tempNode) {
-			System.out.print(deep + " ");
+		while(!q.isEmpty()) {
+			int temp = q.remove();
+			
+			for(int node : tree[temp]) {
+				if(check[node] == false) {
+					q.add(node);
+//					System.out.print(node + " ");
+					check[node] = true;
+					distance[node] += distance[temp] + 1;
+//					System.out.println(node + " 의 거리는 " + distance[node]);
+					max = Math.max(distance[node], max);
+				}
+			}
 		}
 		
+		
+//		for(int i = 1 ; i <= 6; i++) {
+//			for(int temp : tree[i] ) System.out.print("Node " + i + " == " +temp + " ");
+//			System.out.println();
+//		}
+		
+		for(int depth : distance) {
+			if(depth == max) answer ++;
+		}
+		
+		//System.out.print(answer);
 		
 	}
-
 }
+	
